335588e472dcb88698f264ea5107fa900e08e439
mpaxlamitsounas <worldstudy123@gmail.com>
2025-07-21 17:38:39 +0300

    Add UMU_PROTON_DIR config key

--- configuration.py
+++ configuration.py
@@ -1,17 +1,21 @@
 import os
 from pathlib import Path
 
-from classes import DLLOverride
+from classes import Element
 
 PROTON_DIRS: list[str] = [
     os.path.join(Path.home(), ".local/share/Steam/compatibilitytools.d/"),
     os.path.join(Path.home(), ".local/share/umu/compatibilitytools"),
 ]
+# where UMU saves its UMU-Proton versions
+UMU_PROTON_DIR: str = os.path.join(
+    Path.home(), ".local/share/Steam/compatibilitytools.d/"
+)
 DB_NAME: str = "tracking.json"
 DB_DIR: str = os.path.join(Path.home(), ".local/share/umu/compatibilitytools")
 CONFIG_NAME: str = "umu-config.toml"
 PREFIX_DIR: str = os.path.join(Path.home(), ".local/share/wineprefixes/")
 # Label, override string, follow the example
-DLL_OVERRIDES_OPTIONS: list[DLLOverride] = [
-    DLLOverride("winhttp for BepInEx", "winhttp.dll=n;")
+DLL_OVERRIDES_OPTIONS: list[Element] = [
+    Element(info="winhttp for BepInEx", value="winhttp.dll=n;")
 ]
